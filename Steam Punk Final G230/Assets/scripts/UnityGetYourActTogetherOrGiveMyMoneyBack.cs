77ab0948a77666e98826c7655dc2c4cf2809a134
DESKTOP-1SKHOFF\scott <scott@beancurvestudio.com>
2018-04-30 19:16:51 -0700

    Added work around for Prefabs.  Added sample textures to some of the object in the Main scene.

---
+++ Steam Punk Final G230/Assets/scripts/UnityGetYourActTogetherOrGiveMyMoneyBack.cs
@@ -0,0 +1,22 @@
+﻿// Patch to address strange issue with Prefabs
+using UnityEngine.EventSystems;
+using UnityEngine;
+[ExecuteInEditMode]
+public class UnityGetYourActTogetherOrGiveMyMoneyBack : MonoBehaviour
+{
+	public Vector3 m_anchoredPosition;
+	void Start()
+	{
+		GetComponent<RectTransform>().anchoredPosition = m_anchoredPosition;
+	}
+#if UNITY_EDITOR
+	void Update()
+	{
+		if (Application.isPlaying)
+		{
+			return;
+		}
+		m_anchoredPosition = GetComponent<RectTransform>().anchoredPosition3D;
+	}
+#endif
+}
